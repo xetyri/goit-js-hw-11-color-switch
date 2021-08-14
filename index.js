facc06499ce968a5b91ba77cfdd96114833c9069
Roman Leshchenko <xetyri2014@gmail.com>
2021-08-14 10:50:00 +0300

    Добавляет решение задания

--- index.js
+++ index.js
@@ -7,6 +7,37 @@ const colors = [
   '#795548',
 ];
 
-const randomIntegerFromInterval = (min, max) => {
+const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let Clic = null;
+
+const Start = document.querySelector('[data-action="start"]');
+const Stop = document.querySelector('[data-action="stop"]');
+
+Stop.disabled = true;
+
+function StartBtn(event) {
+  event.target.disabled = true;
+  Stop.disabled = false;
+  Clic = setInterval(BgColor, 1000);
+  //Clic = setInterval(document.body.style.backgroundColor = colors[random(0, colors.length)], 500);
+}
+
+function BgColor() {
+  document.body.style.backgroundColor = colors[random(0, colors.length)];
+}
+
+function StopBtn(event) {
+  event.target.disabled = true;
+  Start.disabled = false;
+
+  document.body.style.backgroundColor = '#FFFFFF';
+
+  clearInterval(Clic);
+}
+
+Start.addEventListener('click', StartBtn);
+Stop.addEventListener('click', StopBtn);
+
